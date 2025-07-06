3f5ce30a07af1d81d2a5c66de2c6d3a96534c712
정윤아 <yuna@jeong-yun-aui-MacBookAir.local>
2025-07-06 23:04:59 +0900

    init : spring intialize

---
+++ src/test/java/com/rudolph/Weevo/WeevoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudolph.Weevo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeevoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
