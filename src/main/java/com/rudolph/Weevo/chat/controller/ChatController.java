cf664b56e8753e07bbd9176bf070a377eaa53e8f
s2eojeong <155219307+s2eojeong@users.noreply.github.com>
2025-08-05 17:16:55 +0900

    fix: 카프카 브로커 설정

--- src/main/java/com/rudolph/Weevo/chat/controller/ChatController.java
+++ src/main/java/com/rudolph/Weevo/chat/controller/ChatController.java
@@ -26,7 +26,7 @@ import java.security.Principal;
 @RequestMapping("/chat")
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+//    private final SimpMessageSendingOperations messagingTemplate;
     private final ChatService chatService;
     private final ChatRoomService chatRoomService;
     private final KafkaProducer producer;
@@ -35,7 +35,8 @@ public class ChatController {
     public void sendMessage(ChatMessage message, Principal principal) {
         Chat chat = chatService.saveMessage(message, principal);
         ChatMessage responseDto = ChatMessage.from(chat, message.getReceiverId());
-        messagingTemplate.convertAndSend("/sub/chat/" + message.getChatRoomId(), responseDto);
+//        messagingTemplate.convertAndSend("/sub/chat/" + message.getChatRoomId(), responseDto);
+        producer.sendMessage(responseDto);
     }
 
     // 채팅방 존재 여부
